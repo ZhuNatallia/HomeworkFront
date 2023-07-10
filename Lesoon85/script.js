192de0873ff9ee7c03fc6b16a506436706bd7128
My Name <borisenko_07@mail.ru>
2023-07-10 14:32:44 +0200

    lesson 85

---
+++ Lesoon85/script.js
@@ -0,0 +1,15 @@
+let age = '30';
+age = '18';
+console.log(age);
+
+function date() {
+	let useName = 'Olga';
+}
+/* console.log(username); */
+//ошибка, потому что переменная let вызывается вне зоны видимости
+
+function color() {
+	var titleColor = 'black';
+}
+/* console.log(titleColor); */
+//ошибка, потому что переменная var вызывается вне зоны видимости
